14f724f76ceb5a9507053b650bb33a9b6ade1a78
Esssssss31 <117720707+Esssssss31@users.noreply.github.com>
2023-04-07 16:48:14 +0800

    Update routes.py

--- api/routes.py
+++ api/routes.py
@@ -1,3 +1,5 @@
+# This file contains the API endpoints and their corresponding functions.
+
 class Route:
     def __init__(self, route_id: int, name: str, stops: List[str]):
         self.route_id = route_id
