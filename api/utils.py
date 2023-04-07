149756f1b5585a9d983cfe172d9ac1458973b36c
Esssssss31 <117720707+Esssssss31@users.noreply.github.com>
2023-04-07 16:04:31 +0800

    Create utils.py

---
+++ api/utils.py
@@ -0,0 +1,7 @@
+def get_unit_location(unit_id: int) -> Tuple[float, float]:
+    # code to retrieve unit location from the database
+    return lat, long
+
+def get_route_stops(route_id: int) -> List[str]:
+    # code to retrieve route stops from the database
+    return stops
