b9ddf1930bfb405fa77a4bc5e49cd9619946df90
Esssssss31 <117720707+Esssssss31@users.noreply.github.com>
2023-04-07 16:04:53 +0800

    Create data_access.py

---
+++ api/data_access.py
@@ -0,0 +1,24 @@
+import pymongo
+
+class DataAccess:
+    def __init__(self, connection_string: str):
+        self.client = pymongo.MongoClient(connection_string)
+        self.db = self.client["transit_db"]
+        self.units = self.db["units"]
+        self.routes = self.db["routes"]
+
+    def get_unit_location(self, unit_id: int) -> Tuple[float, float]:
+        # code to retrieve unit location from the database
+        return lat, long
+
+    def get_route_stops(self, route_id: int) -> List[str]:
+        # code to retrieve route stops from the database
+        return stops
+
+    def save_unit_location(self, unit_id: int, lat: float, long: float):
+        # code to save unit location to the database
+        pass
+
+    def save_route(self, route_id: int, name: str, stops: List[str]):
+        # code to save route to the database
+        pass
