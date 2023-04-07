eef0981940723e76419eb431df19836cfb207638
Esssssss31 <117720707+Esssssss31@users.noreply.github.com>
2023-04-07 16:03:37 +0800

    Create __init__.py

---
+++ api/__init__.py
@@ -0,0 +1,6 @@
+class Unit:
+    def __init__(self, unit_id: int, name: str, lat: float, long: float):
+        self.unit_id = unit_id
+        self.name = name
+        self.lat = lat
+        self.long = long
